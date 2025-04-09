f31f2a826f84c4c15a4e8af07c2dfa11e96ae2c3
Jose Martins <josemartins90@gmail.com>
2025-04-09 15:26:53 +0100

    fix intc struct
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/rh850/inc/intc.h
+++ src/arch/rh850/inc/intc.h
@@ -39,9 +39,9 @@ struct intc2 {
     uint8_t  PAD2[3808];            /* 0x1100 - 0x1FDF */
     uint32_t I2EIBG[ARCH_MAX_CPUS];  /* 0x1FE0 - (0x1FE0 + 4*cpu_num - 1) */
     uint8_t  PAD3[PAD3_LEN];        /* (0x1FE0 + 4*cpu_num) - 0x203F */
-    uint32_t EIBD[INTC2_IRQ_NUM];   /* 0x2040 - 0x3FBF */
-    uint8_t  PAD4[128];             /* 0x3FC0 - 0x403F */
-    uint32_t EEIC[INTC2_IRQ_NUM];   /* 0x4040 - 0x5FBF */
+    uint32_t EIBD[INTC2_IRQ_NUM];   /* 0x2080 - 0x3FFF */
+    uint8_t  PAD4[64];              /* 0x4000 - 0x403F */
+    uint32_t EEIC[INTC2_IRQ_NUM];   /* 0x4080 - 0x5FFF */
 };
 
 struct intif {
