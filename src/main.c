9df0de07a8ad8323b59ca501a2ba740d81ec6c28
Jose Martins <josemartins90@gmail.com>
2025-11-11 23:21:35 +0000

    add cva6 32-bit
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/main.c
+++ src/main.c
@@ -30,17 +30,17 @@
 spinlock_t print_lock = SPINLOCK_INITVAL;
 
 void uart_rx_handler(){
-    printf("cpu%d: %s\n",get_cpuid(), __func__);
+    printf("[vm 1]: cpu%d: %s\n",get_cpuid(), __func__);
     uart_clear_rxirq();
 }
 
 void ipi_handler(){
-    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    printf("[vm 1]: cpu%d: %s\n", get_cpuid(), __func__);
     irq_send_ipi(1ull << (get_cpuid() + 1));
 }
 
 void timer_handler(){
-    printf("cpu%d: %s\n", get_cpuid(), __func__);
+    printf("[vm 1]: cpu%d: %s\n", get_cpuid(), __func__);
     timer_set(TIMER_INTERVAL);
     irq_send_ipi(1ull << (get_cpuid() + 1));
 }
@@ -51,7 +51,7 @@ void main(void){
 
     if(cpu_is_master()){
         spin_lock(&print_lock);
-        printf("Bao bare-metal test guest\n");
+        printf("[vm 1]: Bao bare-metal test guest\n");
         spin_unlock(&print_lock);
 
         irq_set_handler(UART_IRQ_ID, uart_rx_handler);
@@ -74,7 +74,7 @@ void main(void){
 
     while(!master_done);
     spin_lock(&print_lock);
-    printf("cpu %d up\n", get_cpuid());
+    printf("[vm 1]:cpu %d up\n", get_cpuid());
     spin_unlock(&print_lock);
 
     while(1) wfi();
---
+++ src/platform/cva6/inc/plat.h
@@ -0,0 +1,14 @@
+#ifndef PLAT_H
+#define PLAT_H
+
+#define PLAT_MEM_BASE 0x84200000
+#define PLAT_MEM_SIZE 0x4000000
+
+#define PLAT_TIMER_FREQ (25000000ull) // 25 MHz
+
+#define PLAT_UART_ADDR (0x10000000)
+#define UART_IRQ_ID (1)
+
+// #define CPU_EXT_SSTC 1
+
+#endif
